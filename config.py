be0399f813a8671c7c593c4f33ce7e1922c0ed71
Freaks <galleyhugo@icloud.com>
2025-11-19 19:48:21 +0100

    feat(Config): Mise en place minimal pour SqlAlchemy

--- config.py
+++ config.py
@@ -1,9 +1,35 @@
 import logging
 import os
 import sys
+
+import pymysql
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from EasyWorkEnv import Config
 
 configuration = Config("variables.json")
+
+
+def config_database():
+    user = configuration.Bdd.User
+    password = configuration.Bdd.Password
+    host = configuration.Bdd.Host
+    port = configuration.Bdd.Port
+    database = configuration.Bdd.DataBase
+
+    connection = pymysql.connect(
+        host=host, user=user, password=password, database=database, port=port
+    )
+    with connection.cursor() as cursor:
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS `{database}`")
+    connection.close()
+
+    engine = create_engine(
+        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    )
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db = session_local()
+    return db
 
 
 def setup_logging():
@@ -40,3 +66,6 @@ def setup_logging():
 
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
+
+
+db = config_database()
