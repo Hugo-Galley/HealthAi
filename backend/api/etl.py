85323261a6ab8e9ca8b7cdbbdbd39307815955c2
isaaclv822 <isaaclda183@gmail.com>
2026-02-24 13:09:05 +0100

    feat(api-etl): ajout d'endpoints pour l'exécution de l'ETL, retour JSON des données non conformes

--- backend/api/etl.py
+++ backend/api/etl.py
@@ -8,10 +8,6 @@ def run_etl_endpoint():
     try:
         result = EtlService().run()
         return result
-    # except Exception:
-    #     raise HTTPException(status_code=500, detail="Erreur lors de l'exécution de l'ETL.")
-    except Exception as e: 
-        import traceback 
-        traceback.print_exc() # Affiche l’erreur réelle dans la console 
-        raise HTTPException(status_code=500, detail=str(e))
+    except Exception:
+        raise HTTPException(status_code=500, detail="Erreur lors de l'exécution de l'ETL.")
 
