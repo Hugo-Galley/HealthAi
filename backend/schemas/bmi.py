67b2a3fa19a7ac62f74cdc3176856a6c95214278
Freaks <galleyhugo@icloud.com>
2026-02-24 12:32:39 +0100

    fix(): suppression des schémas (code mort)

--- backend/schemas/bmi.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from uuid import UUID, uuid4
-
-class BmiResquest(BaseModel):
-    id: UUID = uuid4()
-    patient_id: str
-    height: float
-    weight: float
-    score: float
--- backend/schemas/consumption.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from uuid import UUID, uuid4
-
-class ConsumptionRequest(BaseModel):
-    id: UUID = uuid4()
-    food_id: UUID
-    meal_id: UUID
